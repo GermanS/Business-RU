123570aca832a0ee8a443df75f91cf9ef0d57104
GermanS <german.semenkov@gmail.com>
2013-08-17 19:17:47 +0400

    implement validate_okpo()

--- lib/Business/RU/OKPO.pm
+++ lib/Business/RU/OKPO.pm
@@ -7,9 +7,21 @@ use Moose::Role;
 requires 'okpo';
 
 sub validate_okpo {
-    my $self = shift;
+   my $self = shift;
 
-    return;
+   return unless $self -> okpo();
+
+    my $result1 = 0;
+    my $result2 = 0;
+    for ( my $i = 0; $i < length( $self -> okpo() ) - 1; $i ++ ) {
+        $result1 += substr( $self -> okpo(), $i, 1) * ( ( $i + 1 ) % 11 );
+        $result2 += substr( $self -> okpo(), $i, 1) * ( ( $i + 3 ) % 11 );
+    }
+
+    return 
+        ( ( $result1 % 11 ) > 9 )
+            ? substr( $self -> okpo(), length( $self -> okpo() ) - 1, 1 ) == $result2 % 11 % 10
+            : substr( $self -> okpo(), length( $self -> okpo() ) - 1, 1 ) == $result1 % 11;
 }
 
 1;
@@ -27,8 +39,8 @@ use Moose;
 has 'okpo' => ( is => 'ro', isa => 'Int' );
 with 'Business::RU::OKPO';
 
-my $decorator = myDecorator -> new( okpo => 123456789 );
-if( $decorator -> validate_okpo() ) {
+my $decorator = myDecorator  ->  new( okpo => 123456789 );
+if( $decorator  ->  validate_okpo() ) {
     ... success ...
 } else {
     ... process error ...
